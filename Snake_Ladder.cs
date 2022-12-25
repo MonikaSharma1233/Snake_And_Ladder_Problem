7bec331da848dbbaa33b93bb251610ce8f3ac26b
firsttoken <monikasharma12330@gmail.com>
2022-12-25 17:05:58 +0530

    Username_position0

---
+++ Snake_Ladder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder_Problem
+{
+    public class Snake_Ladder
+    {
+        public static void Snake_and_Ladder()
+        {
+            int start = 0;
+            //UC1
+            Console.WriteLine("WELCOME TO SNAKE AND LADDER.");
+            Console.WriteLine("Enter Player 1 Name: ");
+            String player1 = Console.ReadLine();
+            Console.WriteLine("Hello " + player1 + " Welcome to Snake and Ladder.");
+            Console.WriteLine("Player 1 on: " + start + " Position.");
+
+        }
+    }
+}
