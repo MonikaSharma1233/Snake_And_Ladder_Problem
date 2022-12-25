477a25cf45829071081ad0c4b1d2b6ddf288d28a
firsttoken <monikasharma12330@gmail.com>
2022-12-25 17:02:29 +0530

    Program file _main Method

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Snake_and_Ladder_Problem;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Snake_Ladder sl = new Snake_Ladder();
+        sl.Snake_and_Ladder();  
+       
+        Console.ReadKey();  
+    }
+}
